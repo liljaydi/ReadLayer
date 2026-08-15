f1f618a40e273063a27dc215a090cd83310ac063
Gyde <gydetaparan@gmail.com>
2026-08-15 18:38:37 +0800

    added background image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import MainContent from './components/MainContent'
 import Header from './components/Header'
+import backgroundImage from './assets/AdobeStock_722269965.jpeg'
 
 function App() {
   return (
     <>
+      <img class="background-img" src={backgroundImage}/>
       <Header/>
       <MainContent/>
     </>
